3cf206940b74b1501a71d67f6deb02ba337a0087
Hari Kumpatla <harikump@infinite.com>
2024-10-28 23:35:50 +0530

    added Basic

---
+++ DotNet_tarining/CSharp/Day1_Basic/Day1_Basic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.WriteLine("This is a Basic Program");
+            Console.Read();
+        }
+    }
+}

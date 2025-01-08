f05a2b9ebd9bf966aa8c1a34009ceaad1c31bd57
Hari Kumpatla <harikump@infinite.com>
2025-01-08 11:15:51 +0530

    WEBAPI Assessment1

---
+++ DotNet_tarining/WEBAPI/Assessment1/Assessment1/Controllers/CountryController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Assessment1.Models;
+
+namespace Assessment1.Controllers
+{
+    [RoutePrefix("api/User")]
+    public class CountryController : ApiController
+    {
+        private static List<Country> countries = new List<Country>
+        {
+            new Country { ID = 1, CountryName = "India", Capital = "New Delhi" },
+            new Country { ID = 2, CountryName = "South Africa", Capital = "CapeTown" }
+        };
+
+        [HttpGet]
+        public IHttpActionResult GetAllCountries()
+        {
+            return Ok(countries);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetCountry(int id)
+        {
+            var country = countries.FirstOrDefault(c => c.ID == id);
+            if (country == null)
+                return NotFound();
+
+            return Ok(country);
+        }
+
+        [HttpPost]
+        public IHttpActionResult AddCountry(Country country)
+        {
+            if (country == null)
+                return BadRequest("Invalid data.");
+
+            countries.Add(country);
+            return Created($"api/Country/{country.ID}", country);
+        }
+
+        [HttpPut]
+        public IHttpActionResult UpdateCountry(int id, Country updatedCountry)
+        {
+            var country = countries.FirstOrDefault(c => c.ID == id);
+            if (country == null)
+                return NotFound();
+
+            country.CountryName = updatedCountry.CountryName;
+            country.Capital = updatedCountry.Capital;
+            return Ok(country);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteCountry(int id)
+        {
+            var country = countries.FirstOrDefault(c => c.ID == id);
+            if (country == null)
+                return NotFound();
+
+            countries.Remove(country);
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+    }
+}
